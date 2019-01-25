4dd3381c9282c37bffab120174240db77473189f
Sam Hill <samx7a@gmail.com>
2019-01-25 15:19:13 +0000

    Move main class functionality into grid class. Add constructor to Point class

--- src/Grid.java
+++ src/Grid.java
@@ -5,15 +5,21 @@ import java.util.ArrayList;
  *  Main class for GameOfLife, creates a loop which listens for input to iterate the game.
  *  Starts with an automatically pre-poulated list of live cells.
  */
-public class Main {
+public class Grid {
+	// Note the origin of the grid corresponds to the top left corner.
 	// Grid contains the list of living cells.
-	private static ArrayList<Point> grid = new ArrayList<Point>();
+	private ArrayList<Point> grid = new ArrayList<Point>();
+	public Grid() {
+		// Instantiate grid with default set of points.
+		this.grid = new ArrayList<Point>();
+	}
+	
 	// Update these values as we populate the grid list. They are used for rendering purposes.
 	private int maxY = 0;
 	private int maxX = 0;
 
 	
 	public static void main(String[] args) {
-		
+		Grid grid = new Grid();
 	}
 }
