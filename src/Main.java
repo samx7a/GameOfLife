0b88bd1b03f9f4fadc54eecdd72e78d95252fafc
Sam Hill <samx7a@gmail.com>
2019-01-25 14:57:00 +0000

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+/**
+ * 
+ * @author sam
+ *  Main class for GameOfLife, creates a loop which listens for input to iterate the game.
+ *  Starts with an automatically pre-poulated list of live cells.
+ */
+public class Main {
+	// Grid contains the list of living cells.
+	private static ArrayList<Point> grid = new ArrayList<Point>();
+	// Update these values as we populate the grid list. They are used for rendering purposes.
+	private int maxY = 0;
+	private int maxX = 0;
+
+	
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ src/Point.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ * @author sam
+ * Class to represent a point in the grid inhabited by a cell.
+ */
+public class Point {
+	private int x = 0;
+	private int y = 0;
+	
+	public int getX() {
+		return x;
+	}
+	public int getY() {
+		return y;
+	}
+}
